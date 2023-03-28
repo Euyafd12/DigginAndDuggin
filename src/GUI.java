79b9e272c7413a5f07413ff2134cf7106e260d3e
Eyad Aboelnaga <S929030@cfisd.loc>
2023-03-28 12:58:16 -0500

    asdds

--- src/GUI.java
+++ src/GUI.java
@@ -2,23 +2,21 @@ import javax.sound.sampled.*;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.PrintStream;
-import java.io.PrintWriter;
+import java.io.*;
 import java.util.*;
 
 
 public class GUI extends JPanel implements KeyListener {
 
     private Graphics2D g2d;
-
-    public int xPos, yPos, score, highScore;
+    public int xPos, yPos;
     public final int width, height, groundHeight, velocity;
+    private double highScore, score;
+    private final double tempHiScore;
+    private final Image imgBackground, imgICN, imgLogo, imgHiScore, imgScore;
+    private Image imgDigDug;
     private final ArrayList<Point> path;
-    private Image imgBackground, imgDigDug, imgICN, imgLogo;
-    private Map<String, Image> scoreMap;
-    //private PrintWriter writer;
+    private final Map<String, Image> scoreMap;
 
     public GUI() throws FileNotFoundException {
 
@@ -30,29 +28,25 @@ public class GUI extends JPanel implements KeyListener {
         xPos = 0;
         yPos = 0;
         score = 0;
-        highScore = 67;
 
-        /*
-        writer = new PrintWriter("highscore.txt");
-        Scanner s = new Scanner(new File("highscore.txt"));
-        System.out.println(s.hasNext());
-        highScore = s.nextInt();
-        */
-
-
+        highScore = new Scanner(new File("highscore.txt")).nextDouble();
+        tempHiScore = highScore;
 
         path = new ArrayList<>();
         scoreMap = new HashMap<>();
 
+
+        //Create Map of Scoreboard Number Icons linked to Integers
         for (int i = 0; i < 10; i++) {
             scoreMap.put(String.valueOf(i), new ImageIcon(Objects.requireNonNull(getClass().getResource("Assets/Score" + i + ".png"))).getImage());
         }
 
         imgBackground = new ImageIcon(Objects.requireNonNull(getClass().getResource("Assets/background.png"))).getImage();
         imgDigDug = new ImageIcon(Objects.requireNonNull(getClass().getResource("Assets/SpriteRight.png"))).getImage();
-        imgICN = new ImageIcon(Objects.requireNonNull(getClass().getResource("Assets/DigDugIcon.png"))).getImage();
+        imgICN = new ImageIcon(Objects.requireNonNull(getClass().getResource("Assets/DigDugIcon.jpg"))).getImage();
         imgLogo = new ImageIcon(Objects.requireNonNull(getClass().getResource("Assets/Logo.png"))).getImage();
-
+        imgScore = new ImageIcon(Objects.requireNonNull(getClass().getResource("Assets/imgScore.png"))).getImage();
+        imgHiScore = new ImageIcon(Objects.requireNonNull(getClass().getResource("Assets/imgHi-Score.png"))).getImage();
 
     }
 
@@ -70,6 +64,23 @@ public class GUI extends JPanel implements KeyListener {
         frame.setResizable(false);
         frame.setLocationRelativeTo(null);
 
+        //When [X] is Pressed, Clear the Highscore.txt Files and Replace is With Current Highscore
+        frame.addWindowListener(new WindowAdapter() {
+
+            public void windowClosing(WindowEvent e) {
+
+                if (tempHiScore != highScore) {
+                    try {
+                        PrintWriter pw = new PrintWriter("highscore.txt");
+                        pw.append(String.valueOf(highScore));
+                        pw.close();
+                    } catch (Exception ignored) {
+                    }
+                }
+            }
+        });
+
+        //Start Theme
         audio("Assets/theme.wav");
     }
 
@@ -78,14 +89,16 @@ public class GUI extends JPanel implements KeyListener {
         super.paintComponent(window);
         g2d = (Graphics2D) window;
 
-        //Make black background
+        //Make Black Background
         g2d.setColor(Color.BLACK);
         g2d.fillRect(0, 0, width, height);
 
-        //Dig Dug Logo
-        g2d.drawImage(imgLogo, 100, -10, (int) (1072 / 1.5), (int) (478 / 1.5), null);
+        //Draw Dig Dug UI
+        g2d.drawImage(imgLogo, 100, -10, 714, 318, null);
+        g2d.drawImage(imgScore, 700, 400, 39 * 3, 7 * 3, null);
+        g2d.drawImage(imgHiScore, 700, 500, 46 * 3, 16 * 3, null);
 
-        //Create dirt
+        //Create Dirt
         g2d.drawImage(imgBackground, 0, 300, 181, groundHeight, null);
         g2d.drawImage(imgBackground, 180, 300, 181, groundHeight, null);
         g2d.drawImage(imgBackground, 360, 300, 181, groundHeight, null);
@@ -94,31 +107,46 @@ public class GUI extends JPanel implements KeyListener {
         //Make Single black square on top of Doug
         g2d.fillRect(xPos, yPos, 40, 40);
 
+        //Scoring System
+        if (score < 10000) {
+
+            if (!path.contains(new Point(xPos, yPos))) {
+                score += 0.25;
+            }
+
+            if (score > highScore && score == (int) score) {
+
+                highScore = score;
+            }
+        }
+        drawScore(score);
+
         //Make him move and black path follows
         drawPath();
         moveDigDug();
-        score++;
-        if (score > highScore) highScore = score;
-        //writer.flush();
-        //writer.append("" + highScore);
-        drawScore(score);
+
+
     }
 
-    public void drawScore(int score) {
 
-        String sc = "" + score;
-        String hc = "" + highScore;
+
+    public void drawScore(double score) {
+
+        String sc = "" + (int) score;
+        String hc = "" + (int) highScore;
 
         int xTemp = 750;
         int xxTemp = 750;
 
+        //Search Through Map for Matching Character and Draw it
+
         for (int i = 0; i < sc.length(); i++) {
-            g2d.drawImage(scoreMap.get(sc.substring(i, i+1)), xTemp, 385, 24, 24, null);
+            g2d.drawImage(scoreMap.get(sc.substring(i, i + 1)), xTemp, 445, 24, 24, null);
             xTemp += 24;
         }
 
         for (int i = 0; i < hc.length(); i++) {
-            g2d.drawImage(scoreMap.get(hc.substring(i, i+1)), xxTemp, 385+48, 24, 24, null);
+            g2d.drawImage(scoreMap.get(hc.substring(i, i + 1)), xxTemp, 535 + 48, 24, 24, null);
             xxTemp += 24;
         }
     }
@@ -126,14 +154,18 @@ public class GUI extends JPanel implements KeyListener {
 
     public void moveDigDug() {
 
+        //Draw Dig Dug Position and Save Existing Position
         g2d.drawImage(imgDigDug, xPos, yPos, 40, 40, null);
+
         Point temp = new Point(xPos, yPos);
-        path.add(temp);
+        if (!path.contains(temp)) {
+            path.add(temp);
+        }
     }
+
     public void drawPath() {
 
-        g2d.setColor(Color.BLACK);
-
+        //Go Through ALl Previous Paths and Draw Black Spot
         for (Point p : path) {
             g2d.fillRect(p.x, p.y, 40, 40);
         }
@@ -142,45 +174,43 @@ public class GUI extends JPanel implements KeyListener {
     @Override
     public void keyPressed(KeyEvent e) {
 
+        //Temp Left positions
         Image dL = new ImageIcon(Objects.requireNonNull(getClass().getResource("Assets/SpriteDownLeft.png"))).getImage();
         Image L = new ImageIcon(Objects.requireNonNull(getClass().getResource("Assets/SpriteLeft.png"))).getImage();
         Image uL = new ImageIcon(Objects.requireNonNull(getClass().getResource("Assets/SpriteUpLeft.png"))).getImage();
 
         int key = e.getKeyCode();
 
+        //Check What Key is Pressed, and Compare What the Current Sprite Orientation to Left Facing Positions
         if (key == KeyEvent.VK_UP) {
 
             yPos -= velocity;
 
             if (imgDigDug.equals(dL) || imgDigDug.equals(L) || imgDigDug.equals(uL)) {
                 imgDigDug = new ImageIcon(Objects.requireNonNull(getClass().getResource("Assets/SpriteUpLeft.png"))).getImage();
-            }
-            else {
+            } else {
                 imgDigDug = new ImageIcon(Objects.requireNonNull(getClass().getResource("Assets/SpriteUpRight.png"))).getImage();
             }
-        }
-        else if (key == KeyEvent.VK_DOWN) {
+        } else if (key == KeyEvent.VK_DOWN) {
 
             yPos += velocity;
 
             if (imgDigDug.equals(dL) || imgDigDug.equals(L) || imgDigDug.equals(uL)) {
                 imgDigDug = new ImageIcon(Objects.requireNonNull(getClass().getResource("Assets/SpriteDownLeft.png"))).getImage();
-            }
-            else {
+            } else {
                 imgDigDug = new ImageIcon(Objects.requireNonNull(getClass().getResource("Assets/SpriteDownRight.png"))).getImage();
             }
-        }
-        else if (key == KeyEvent.VK_RIGHT) {
+        } else if (key == KeyEvent.VK_RIGHT) {
 
             xPos += velocity;
             imgDigDug = new ImageIcon(Objects.requireNonNull(getClass().getResource("Assets/SpriteRight.png"))).getImage();
-        }
-        else if (key == KeyEvent.VK_LEFT) {
+        } else if (key == KeyEvent.VK_LEFT) {
 
             xPos -= velocity;
             imgDigDug = new ImageIcon(Objects.requireNonNull(getClass().getResource("Assets/SpriteLeft.png"))).getImage();
         }
 
+        //Keep Dig Dug in Bounds
         if (xPos < 0) xPos = 0;
         if (xPos > 611) xPos = 611;
         if (yPos < 270) yPos = 270;
@@ -191,17 +221,22 @@ public class GUI extends JPanel implements KeyListener {
 
     public void audio(String a) {
 
+        //Take in Audio File Name and Continuously Play the Associated File
         try {
             AudioInputStream system = AudioSystem.getAudioInputStream(Objects.requireNonNull(getClass().getResource(a)));
             Clip sound = AudioSystem.getClip();
             sound.open(system);
             sound.start();
             sound.loop(Clip.LOOP_CONTINUOUSLY);
-        } catch (Exception ignore) {}
+        } catch (Exception ignore) {
+        }
     }
 
     @Override
-    public void keyTyped(KeyEvent e) {}
+    public void keyTyped(KeyEvent e) {
+    }
+
     @Override
-    public void keyReleased(KeyEvent e) {}
+    public void keyReleased(KeyEvent e) {
+    }
 }
